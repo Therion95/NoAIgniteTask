ece79bd8e6d0ea0579ca3a30ab4d81c8f8526f26
Therion95 <piotrpawelczyk95@gmail.com>
2022-06-14 00:46:48 +0200

    Created main file.

--- NoAIgniteTask/Program.cs
+++ NoAIgniteTask/Program.cs
@@ -1,3 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
 
-Console.WriteLine("Hello, World!");
+namespace NoAIgniteTask // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        public static void Main(string[] dates)
+        {
+            DatesOrganiser datesOrganiser = new DatesOrganiser(dates);
+            Console.ReadKey();
+        }
+    }
+}
