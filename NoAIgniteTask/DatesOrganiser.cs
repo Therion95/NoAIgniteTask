c3269ae26a36030fd3b4a8743942ed61eb9220d3
Therion95 <piotrpawelczyk95@gmail.com>
2022-06-14 00:47:24 +0200

    Created DatesOrganiser.cs - brain of whole app

---
+++ NoAIgniteTask/DatesOrganiser.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NoAIgniteTask
+{
+    public class DatesOrganiser
+    {
+        public DatesOrganiser(string[] dates)
+        {
+            DatesValidator datesValidator = new();
+            DateSorter dateSorter = new();
+            try
+            {
+                datesValidator.CheckIfInputEqualsTwo(dates);
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+            var separator = '.';
+            var possibleSeparators = new List<char>() {'.', '/', '-'};
+        
+            foreach (var date in dates)
+            {
+                datesValidator.ValidateDate(date);
+                foreach (var sep in possibleSeparators.Where(date.Contains))
+                {
+                    separator = sep;
+                }
+                Date dateConverted = new (date, separator);
+                dateSorter.ListOfBothDates.Add(dateConverted);
+            }
+            dateSorter.SortDates();
+            Console.WriteLine(dateSorter);
+        }
+    }
+}
